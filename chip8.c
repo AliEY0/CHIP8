9ee038826913764c3e5d60c163c4c8faa30907c9
AliEY0 <ali.elyousfi@outlook.be>
2025-10-04 22:28:49 +0200

    add op code where first nibble = 3,4,5,6,7,8 and 9

--- chip8.c
+++ chip8.c
@@ -25,7 +25,7 @@ void emulateCycle(Chip8 *chip8){
     uint8_t nibble3 = ((opcode >> 4)&0x000F);
     uint8_t nibble4 = ((opcode)&0x000F);
 
-    uint8_t nibble23 = ((opcode>> 4)&0x00FF);
+    uint8_t nibble34 = ((opcode)&0x00FF);
     
     uint16_t nibble234 = ((opcode)&0x0FFF);
 
@@ -47,10 +47,112 @@ void emulateCycle(Chip8 *chip8){
     else if(nibble1 == 2){
         if(chip8->sp < 16){
             chip8->stack[chip8->sp] = chip8->pc;
-            chip8->sp = chip8->sp + 1;
+            chip8->sp += 1;
             chip8->pc = nibble234;     
         }
     }
+    else if(nibble1 == 3){
+        if(chip8->registers[nibble2] == nibble34){
+            chip8->pc += 2;        
+        }
+    }
+    else if(nibble1 == 4){
+        if(chip8->registers[nibble2] != nibble34){
+            chip8->pc += 2;        
+        }
+    }
+    else if (nibble1 == 5){
+        if(chip8->registers[nibble2] == chip8->registers[nibble3] ){
+            chip8->pc += 2;        
+        }
+    }
+    else if(nibble1 == 6){
+        chip8->registers[nibble2] = nibble34;
+    }
+
+    else if(nibble1 == 7){
+        chip8->registers[nibble2] += nibble34;
+    }
+    else if(nibble1 == 8){
+       switch (nibble4) {
+           case 0:
+               chip8->registers[nibble2] = chip8->registers[nibble3];
+               break;
+           case 1:
+               chip8->registers[nibble2] |= chip8->registers[nibble3];
+               break; 
+           case 2:
+               chip8->registers[nibble2] &= chip8->registers[nibble3];
+               break;
+           case 3:
+               chip8->registers[nibble2] ^= chip8->registers[nibble3];
+               break;
+           case 4:{
+                   uint16_t sum = chip8->registers[nibble2] + chip8->registers[nibble3];
+                   if(sum > 255){
+                       chip8->registers[0xF] = 1;
+                   } else{
+                       chip8->registers[0xF] = 0;
+                   }
+                   chip8->registers[nibble2] = (sum & 0x00FF);
+                   break;
+               }
+           case 5:
+               
+                
+               if(chip8->registers[nibble2] >=  chip8->registers[nibble3]){
+                    chip8->registers[0xF] = 1;
+               } else {
+                    chip8->registers[0xF] = 0;
+               }
+               chip8->registers[nibble2] -= chip8->registers[nibble3];
+               break;
+           case 6:
+               
+               chip8->registers[0xF] = (chip8->registers[nibble2] & 0x1);
+               chip8->registers[nibble2] >>= 1;
+               break;
+           case 7:
+               if(chip8->registers[nibble3] >=  chip8->registers[nibble2]){
+                    chip8->registers[0xF] = 1;
+               } else {
+                    chip8->registers[0xF] = 0;
+               }
+               
+               chip8->registers[nibble2] = chip8->registers[nibble3] - chip8->registers[nibble2];
+               
+               break;
+           case 0xE:
+               
+                if(chip8->registers[nibble2] & 0x80){
+                    chip8->registers[0xF] = 1;   
+                } else{
+                    chip8->registers[0xF] = 0;   
+                }
+               chip8->registers[nibble2] <<= 1;
+               break;
+       }
+
+    }
+    else if(nibble1 == 9){
+        if(chip8->registers[nibble2] != chip8->registers[nibble3]){
+             chip8->pc += 2;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
 
--- chip8.h
+++ chip8.h
@@ -1,7 +1,6 @@
 #ifndef CHIP8_H
 #define CHIP8_H
 
-#include <cstdint>
 #include <stdint.h>
 #include <stdbool.h>
 
